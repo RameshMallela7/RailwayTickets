120db12fd9ed6e2490b50d68b97abd726023a467
Ramesh <mallelaramesh5@gmail>
2024-03-14 07:47:47 +0530

    Initial commit

---
+++ src/main/java/com/springboot/RailwayTicket/Model/PassengerModel.java
@@ -0,0 +1,5 @@
+package com.springboot.RailwayTicket.Model;
+
+public class PassengerModel {
+
+}
---
+++ src/main/java/com/springboot/RailwayTicket/RailwayTicketApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.RailwayTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayTicketApplication.class, args);
+	}
+
+}
